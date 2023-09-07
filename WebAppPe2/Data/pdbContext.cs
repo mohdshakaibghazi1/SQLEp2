892e70ea052a4e7000b71a0ba39370cb5096f585
Mohd Shakaib Ghazi <mohd.shakaibghazi@mphasis.com>
2023-09-07 15:12:33 +0530

    first

---
+++ WebAppPe2/Data/pdbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAppPe2.Models;
+
+namespace WebAppPe2.Data
+{
+    public class pdbContext : DbContext
+    {
+        public pdbContext (DbContextOptions<pdbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAppPe2.Models.Order> Order { get; set; } = default!;
+
+        public DbSet<WebAppPe2.Models.OrderDetail>? OrderDetail { get; set; }
+
+        public DbSet<WebAppPe2.Models.Product>? Product { get; set; }
+    }
+}
